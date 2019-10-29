4ceedf9ed21f74b6d71cc191c42b51d77dda0d80
patheagames <admin@pathea.net>
2019-10-29 10:21:32 +0800

    part 4

---
+++ Assets/Resources/Movie/EDRunner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EDRunner : MonoBehaviour 
+{
+	MovieTexture mMovie;
+	// Use this for initialization
+	void Start()
+	{
+		mMovie = GetComponent<UITexture>().mainTexture as MovieTexture;
+		if (mMovie != null) 
+		{
+			mMovie.Stop ();
+			mMovie.Play ();
+			
+			AudioSource sound = gameObject.AddComponent<AudioSource>();
+			sound.clip = mMovie.audioClip;
+			sound.Stop();
+			sound.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(PeInput.Get(PeInput.LogicFunction.OptionsUI) || !mMovie.isPlaying)
+		{
+            Application.LoadLevel("GameCredits");
+        }
+	}
+}
---
+++ Assets/Resources/Movie/IntroRunner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroRunner : MonoBehaviour
+{
+    MovieTexture mMovie;
+
+    void Start()
+    {
+        mMovie = GetComponent<RawImage>().texture as MovieTexture;
+		if (mMovie != null) {
+			mMovie.Stop ();
+			mMovie.Play ();
+
+			AudioSource sound = gameObject.AddComponent<AudioSource>();
+			sound.clip = mMovie.audioClip;
+			sound.Stop();
+			sound.Play();
+		}
+    }
+
+    void Update()
+    {
+		if (mMovie != null && !mMovie.isPlaying)
+        {
+            EndMovie();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)
+            || Input.GetKeyDown(KeyCode.Return)
+            || Input.GetKeyDown(KeyCode.Space)
+            || Input.GetMouseButtonDown(0)) //lz-2016.08.16  功能 #3111片头动画在现有按键跳过基础上增加鼠标左键跳过
+        {
+            EndMovie();
+        }
+    }
+
+    void EndMovie()
+    {
+        if (movieEnd != null)
+        {
+            movieEnd();
+        }
+        enabled = false;
+    }
+
+    public delegate void MovieEnd();
+    public static MovieEnd movieEnd;
+}
