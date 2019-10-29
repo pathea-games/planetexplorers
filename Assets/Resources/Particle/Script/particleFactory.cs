f579a5902c168eeb2f69e00433c4383503f13461
patheagames <admin@pathea.net>
2019-10-29 10:24:42 +0800

    part 4

---
+++ Assets/Resources/Particle/Script/particleFactory.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleFactory : MonoBehaviour {
+	public bool loop;
+	public float cycleTime;
+	public int explodeNum;
+	public float interval;
+	public GameObject unit;
+	public Color color;
+
+	float pastTime = 0f;
+	float unitTime = 0f;
+	int totalNum = 1;
+	GameObject obj;
+	void Start () {
+		obj = Instantiate(unit, transform.position, transform.rotation) as GameObject;
+		obj.GetComponent<ParticleSystem>().startColor = color;
+		obj.GetComponent<ParticleSystem>().startRotation = Vector3.Angle(transform.forward,Vector3.forward) / 57.29578f * Mathf.Sign(transform.forward.x);
+		obj.transform.parent = transform;
+	}
+	
+
+	void FixedUpdate () {
+		if(!loop)
+		{
+			pastTime += Time.deltaTime;
+			if(pastTime > cycleTime)
+			{
+				pastTime = 0f;
+				obj = Instantiate(unit, transform.position, transform.rotation) as GameObject;
+				obj.GetComponent<ParticleSystem>().startColor = color;
+				obj.GetComponent<ParticleSystem>().startRotation = Vector3.Angle(transform.forward,Vector3.forward) / 57.29578f * Mathf.Sign(transform.forward.x);
+				obj.transform.parent = transform;
+				totalNum = 1;
+				unitTime = -Time.deltaTime;
+			}
+			if(totalNum >= explodeNum)
+					return;
+		}
+		if(unitTime >= interval)
+		{
+			obj = Instantiate(unit, transform.position, transform.rotation) as GameObject;
+			obj.GetComponent<ParticleSystem>().startColor = color;
+			obj.GetComponent<ParticleSystem>().startRotation = Vector3.Angle(transform.forward,Vector3.forward) / 57.29578f * Mathf.Sign(transform.forward.x);
+			obj.transform.parent = transform;
+			unitTime -= interval;
+			totalNum ++;
+		}
+		else
+			unitTime += Time.deltaTime;
+
+
+
+	}
+}
---
+++ Assets/Resources/Particle/Script/particleTrajectory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class particleTrajectory : MonoBehaviour {
+
+	public Vector3 speed;
+	public float lifeTime;
+
+	void Start () {
+		Invoke("dest",lifeTime);
+	}
+
+	void FixedUpdate () {
+		transform.position += speed * Time.deltaTime;
+	}
+
+	void dest(){
+		Destroy(gameObject);
+	}
+}
